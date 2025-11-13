64ee225cf546e7d09617c0d44fc62d446637e9a8
olimp <abrakadaniel@mail.ru>
2025-11-13 20:18:16 +0300

    CORS support

--- ImageStorage/internal/app/app.go
+++ ImageStorage/internal/app/app.go
@@ -123,6 +123,22 @@ func (a *Application) initDB() error {
 	return nil
 }
 
+func corsMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+        w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+        if r.Method == http.MethodOptions {
+            w.WriteHeader(http.StatusNoContent)
+            return
+        }
+
+        next.ServeHTTP(w, r)
+    })
+}
+
 func (a *Application) initServer() error {
 	repo := repository.NewRepository(a.db)
 	tgClient := tg.NewClient(a.cfg.TelegramBotToken)
@@ -143,7 +159,7 @@ func (a *Application) initServer() error {
 
 	a.server = &http.Server{
 		Addr:    a.cfg.ServerURL,
-		Handler: mux,
+		Handler: corsMiddleware(mux),
 	}
 
 	a.wg.Add(1)
