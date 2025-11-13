ec100851bd131e889e764543b9336ee64e30d356
Olimp666 <abrakadaniel@mail.ru>
2025-11-13 03:29:07 +0300

    The Refactoring (of bot)

--- MemeVaultControl/src/Client/BackendClient.cs
+++ MemeVaultControl/src/Client/BackendClient.cs
@@ -1,37 +1,41 @@
 using MemeVaultControl.Helpers;
 using MemeVaultControl.Model;
 using Newtonsoft.Json;
+using RestSharp;
 
 namespace MemeVaultControl.Client;
 
 public class BackendClient
 {
-    private readonly HttpClient _client = new();
-    private readonly string _serverUrl = ConfigHelper.Endpoint;
+    private readonly RestClient _client = new(ConfigHelper.Endpoint);
 
-    public async Task UploadImage(UploadRequest uploadRequest)
+    public async Task UploadImage(UploadRequest body)
     {
-        var body = new StringContent(JsonConvert.SerializeObject(uploadRequest));
-        var response = await _client.PostAsync(
-            _serverUrl + $"/upload?user_id={uploadRequest.UserId}" +
-            $"&tg_file_id={uploadRequest.Image}" +
-            $"&file_type={JsonConvert.SerializeObject(uploadRequest.MediaType).Replace("\"", "")}",
-            body
-        );
-
-        response.EnsureSuccessStatusCode();
+        var request = new RestRequest("/upload", Method.Post);
+        request.AddQueryParameter("user_id", body.UserId);
+        request.AddQueryParameter("tg_file_id", body.Image);
+        request.AddQueryParameter("file_type", body.MediaType.ToString().ToLower());
+        request.AddJsonBody(new { tags = body.Tags });
+        var response = await _client.ExecuteAsync(request);
+        
+        Console.WriteLine($"Executed /upload with status code {response.StatusCode}");
     }
 
-    public async Task<ListResponse?> GetList(ListRequest listRequest)
+    public async Task<TagSearchResponse?> SearchByTags(TagSearchRequest body)
     {
-        var body = new StringContent(JsonConvert.SerializeObject(listRequest));
-        var response = await _client.PostAsync(
-            _serverUrl + $"/images?user_id={listRequest.UserId}",
-            body
-        );
+        var request = new RestRequest("/images", Method.Post);
+        request.AddQueryParameter("user_id", body.UserId);
+        var parsedTags = body.Tags;
+        request.AddJsonBody(new { tags = parsedTags });
+        var response = await _client.ExecuteAsync<TagSearchResponse>(request);
+        return response.Data;
+    }
 
-        var content = await response.Content.ReadAsStringAsync();
-        response.EnsureSuccessStatusCode();
-        return JsonConvert.DeserializeObject<ListResponse>(content);
+    public async Task<ListUserMediaResponse?> ListUserMedia(ListUserMediaRequest body)
+    {
+        var request = new RestRequest("/user/images");
+        request.AddQueryParameter("user_id", body.UserId);
+        var response = await _client.ExecuteAsync<ListUserMediaResponse>(request);
+        return response.Data;
     }
 }
--- MemeVaultControl/src/Model/MediaEntry.cs
+++ MemeVaultControl/src/Model/MediaEntry.cs
@@ -1,10 +1,10 @@
-using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
 namespace MemeVaultControl.Model;
 
-[JsonObject]
 public class MediaEntry
 {
-    [JsonProperty("tg_file_id")] public string FileId { get; set; }
-    [JsonProperty("file_type")] public MediaType MediaType { get; set; }
+    [JsonPropertyName("tg_file_id")] public string FileId { get; set; }
+    [JsonPropertyName("file_type")] public MediaType MediaType { get; set; }
+    [JsonPropertyName("tags")] public List<string> Tags { get; set; }
 }
